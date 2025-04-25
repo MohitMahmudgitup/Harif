a6fcc890e0a89278717b17f03362ffa359c60648
MohitMahmudgitup <mohitmahmud25@gmail.com>
2025-04-26 00:09:32 +0600

    add

--- fontend/src/compunents/Cart/Cart.jsx
+++ fontend/src/compunents/Cart/Cart.jsx
@@ -4,7 +4,7 @@ const Cart = () => {
   return (
     <div className='cart'>
         <div className='cart-img-container'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDISJF2ZH9KHtiSUtbHnG_gyTjAx9k3uBtYw&s" alt="" />
+            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/934501151218465.6307c5362ac88.png" alt="" />
         </div>
         <div className='cart-info-container'>
             <h1>Product Name</h1>
